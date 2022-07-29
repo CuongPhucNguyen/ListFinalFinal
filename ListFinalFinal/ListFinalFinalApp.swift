e5b9891c3c9f6bf0088c7501c08bc1a821a1ee83
Cuong <s3881006@rmit.edu.vn>
2022-07-29 13:39:28 +0700

    Initial Commit

---
+++ ListFinalFinal/ListFinalFinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListFinalFinalApp.swift
+//  ListFinalFinal
+//
+//  Created by Cuong Nguyen Phuc on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct ListFinalFinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
