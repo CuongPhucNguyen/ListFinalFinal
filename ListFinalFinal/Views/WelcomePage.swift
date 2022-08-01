da783ec71054b0b75a80088fe2c295878603d30d
Cuong Nguyen <s3881006@rmit.edu.vn>
2022-08-01 19:02:11 +0700

    welcome page animation

--- ListFinalFinal/Views/WelcomePage.swift
+++ ListFinalFinal/Views/WelcomePage.swift
@@ -14,19 +14,26 @@ struct welcomePage: View {
     @State var rectOpacity: Double = 0.0
     @State var listForground: Double = 0.0
     @State var size: Double = 1
+    @State var listOpacity: Double = 0.0
     var body: some View{
         ZStack{
-            
             Color(red: 0.03, green: 0.03, blue: 0.35)
                 .edgesIgnoringSafeArea(.all)
-            Image("rmit-logo")
-                .opacity(rectOpacity)
+            
             stuffList()
                 .clipped()
+                .mask(
+                    Image("rmit-logo")
+                        .opacity(rectOpacity)
+                )
+                .opacity(<#T##Double#>)
+            Image("rmit-logo")
+                .opacity(rectOpacity)
         }
         .onAppear{
             withAnimation(.easeIn(duration: 1)){
                 rectOpacity += 1.0
+                listOpacity += 1.0
             }
         }
                 
