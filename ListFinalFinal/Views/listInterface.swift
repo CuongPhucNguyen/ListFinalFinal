261032a852f9cdc9772df9c1d36dc9adb32a1905
Cuong Nguyen <s3881006@rmit.edu.vn>
2022-08-02 14:33:24 +0700

    List row animation

--- ListFinalFinal/Views/listInterface.swift
+++ ListFinalFinal/Views/listInterface.swift
@@ -16,7 +16,7 @@ import SwiftUI
 struct Stuff: View {
     @State var stuff: StuffObject
     @State var delayIn: Int
-    @State var moveIn: Double = 0.0
+    @State var moveIn: Double = UIScreen.main.bounds.width
     var body: some View {
         VStack(alignment: .leading){
             HStack{
@@ -32,13 +32,13 @@ struct Stuff: View {
                     .font(.subheadline)
             }
         }
-        .padding()
-        .onAppear(
-            perform: withAnimation(.easeOut(duration: 0.5).delay(Double(delayIn) + 0.5)){
-                moveIn = UIScreen.main.bounds.width
+        .padding(.leading,moveIn)
+        .onAppear{
+                withAnimation(.easeOut(duration: 0.5).delay(Double(delayIn-1) + 0.2)){
+                moveIn -= UIScreen.main.bounds.width
             }
-        )
-        .padding(.leading, UIScreen.main.bounds.width - moveIn)
+        }
+        
         
     }
     
